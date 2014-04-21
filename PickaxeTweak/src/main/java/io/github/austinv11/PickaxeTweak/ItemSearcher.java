c2be881862fbe98c798de7fd1e62084d64932d1c
austinv11 <austinv11@gmail.com>
2014-04-21 12:04:46 -0400

    now defaults to torches if nothing else is present
    (nothing if torches aren’t present)

--- PickaxeTweak/src/main/java/io/github/austinv11/PickaxeTweak/ItemSearcher.java
+++ PickaxeTweak/src/main/java/io/github/austinv11/PickaxeTweak/ItemSearcher.java
@@ -6,11 +6,15 @@ import org.bukkit.entity.Player;
 public class ItemSearcher {
 	Material[] blacklist = {Material.DIAMOND_PICKAXE};//TOOD update
 	public int getUsableItem(Player player, int slot){
-		for (int i = slot; i < 9; i++){
-			for (int j = 0; j < blacklist.length; j++){
-				if (player.getInventory().getItem(i).getType() != blacklist[j]){
-					return i;
-				}
+		int k = slot + 1;
+		for (int j = 0; j < blacklist.length; j++){
+			if (player.getInventory().getItem(k).getType() != blacklist[j]){
+				return k;
+			}
+		}
+		for (int i =  slot; i < 9; i++){
+			if (player.getInventory().getItem(i).getType() == Material.TORCH){
+				return i;
 			}
 		}
 		return 0;
