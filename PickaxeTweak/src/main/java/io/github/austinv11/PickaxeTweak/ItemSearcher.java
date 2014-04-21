7941374470a05da9c850b5effff27af43a51626f
austinv11 <austinv11@gmail.com>
2014-04-21 12:10:22 -0400

    Let's try this again...

--- PickaxeTweak/src/main/java/io/github/austinv11/PickaxeTweak/ItemSearcher.java
+++ PickaxeTweak/src/main/java/io/github/austinv11/PickaxeTweak/ItemSearcher.java
@@ -4,17 +4,22 @@ import org.bukkit.Material;
 import org.bukkit.entity.Player;
 
 public class ItemSearcher {
-	Material[] blacklist = {Material.DIAMOND_PICKAXE};//TOOD update
+	Material[] blacklist = {Material.DIAMOND_PICKAXE, Material.AIR};//TOOD update
 	public int getUsableItem(Player player, int slot){
 		int k = slot + 1;
+		boolean r = false;
 		for (int j = 0; j < blacklist.length; j++){
 			if (player.getInventory().getItem(k).getType() != blacklist[j]){
-				return k;
+				r = true;
 			}
 		}
-		for (int i =  slot; i < 9; i++){
-			if (player.getInventory().getItem(i).getType() == Material.TORCH){
-				return i;
+		if (r == true){
+			return k;
+		}else{
+			for (int i =  slot; i < 9; i++){
+				if (player.getInventory().getItem(i).getType() == Material.TORCH){
+					return i;
+				}
 			}
 		}
 		return 0;
