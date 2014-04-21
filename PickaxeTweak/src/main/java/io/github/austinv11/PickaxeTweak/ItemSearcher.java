e6f4d7db77b896d70edf0db6919582b5c018e04b
austinv11 <austinv11@gmail.com>
2014-04-21 12:13:59 -0400

    one more time

--- PickaxeTweak/src/main/java/io/github/austinv11/PickaxeTweak/ItemSearcher.java
+++ PickaxeTweak/src/main/java/io/github/austinv11/PickaxeTweak/ItemSearcher.java
@@ -7,10 +7,12 @@ public class ItemSearcher {
 	Material[] blacklist = {Material.DIAMOND_PICKAXE, Material.AIR};//TOOD update
 	public int getUsableItem(Player player, int slot){
 		int k = slot + 1;
-		boolean r = false;
+		boolean r = true;
 		for (int j = 0; j < blacklist.length; j++){
-			if (player.getInventory().getItem(k).getType() != blacklist[j]){
-				r = true;
+			if (player.getInventory().getItem(k) == null){
+				r = false;
+			}else if (player.getInventory().getItem(k).getType() == blacklist[j]){
+				r = false;
 			}
 		}
 		if (r == true){
